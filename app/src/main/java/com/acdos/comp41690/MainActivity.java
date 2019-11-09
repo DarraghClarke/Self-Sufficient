49d0e2f329417cfa56f22538b5ff92d300bb6371
Oisin Quinn <oisin1001@gmail.com>
2019-11-09 18:37:18 +0000

    Removed ContentResolver logic because we don't need it and it needlessly complicates the app

--- app/src/main/java/com/acdos/comp41690/MainActivity.java
+++ app/src/main/java/com/acdos/comp41690/MainActivity.java
@@ -1,7 +1,17 @@
 package com.acdos.comp41690;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.acdos.comp41690.data.WaterTrackingContract.WaterTrackingEntry;
+import com.acdos.comp41690.data.WaterTrackingDbHelper;
+
+import java.time.Instant;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WaterTrackingDbHelper dbHelper = new WaterTrackingDbHelper(this);
+
+        ContentValues values = new ContentValues();
+        values.put(WaterTrackingEntry.COLUMN_NAME_VOLUME, 53302.33);
+        values.put(WaterTrackingEntry.COLUMN_NAME_TIMESTAMP, Instant.now().getEpochSecond());
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        db.insert(WaterTrackingEntry.TABLE_NAME, null, values);
+
+        String[] projection = {
+                WaterTrackingEntry._ID,
+                WaterTrackingEntry.COLUMN_NAME_TIMESTAMP,
+                WaterTrackingEntry.COLUMN_NAME_VOLUME };
+
+        Cursor c = db.query(WaterTrackingEntry.TABLE_NAME, projection, null, null, null, null, null);
+
+        while(c.moveToNext()) {
+            Log.d("MainActivity", c.getLong(0) + ", " + c.getLong(1) + ", " + c.getDouble(2));
+        }
+        c.close();
     }
 }
--- app/src/main/java/com/acdos/comp41690/data/WaterTrackingContract.java
+++ app/src/main/java/com/acdos/comp41690/data/WaterTrackingContract.java
@@ -1,32 +1,20 @@
 package com.acdos.comp41690.data;
 
-import android.content.ContentResolver;
-import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
  * Created by Oisin Quinn (@oisin1001) on 2019-11-08.
  */
-public class WaterTrackingContract {
+public final class WaterTrackingContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private WaterTrackingContract() {}
 
-    public static final String CONTENT_AUTHORITY = "com.acdos.comp41690";
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-    public static final String PATH_WATER= "water";
-
-    public static abstract class WaterTrackingEntry implements BaseColumns {
-        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_WATER);
-
-        public static final String _ID = BaseColumns._ID;
+    /* Inner class that defines the table contents */
+    public static class WaterTrackingEntry implements BaseColumns {
         public static final String TABLE_NAME = "water";
-        public static final String COLUMN_VOLUME = "volume";
-        public static final String COLUMN_TIMESTAMP = "timestamp";
-
-
-        public static final String CONTENT_LIST_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WATER;
-
-
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WATER;
+        public static final String COLUMN_NAME_VOLUME = "volume";
+        public static final String COLUMN_NAME_TIMESTAMP = "timestamp";
     }
 }
+
