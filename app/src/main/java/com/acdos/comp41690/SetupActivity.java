996c43db586ce24c83e33b2c316d368bd0368583
Oisin Quinn <oisin1001@gmail.com>
2019-11-09 19:28:25 +0000

    Added logic for deteecting first launch and some dummy activities/layout

---
+++ app/src/main/java/com/acdos/comp41690/SetupActivity.java
@@ -0,0 +1,14 @@
+package com.acdos.comp41690;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}
