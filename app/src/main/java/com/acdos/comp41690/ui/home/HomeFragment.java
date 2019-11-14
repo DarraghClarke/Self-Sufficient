c276364b7bc72ed2428c3e9249f017d26cec2c2d
charliekelly13 <charleskelly13@gmail.com>
2019-11-14 21:17:39 +0000

    Implementing home screen
    Two temporary images and links to other fragments. Needs testing

--- app/src/main/java/com/acdos/comp41690/ui/home/HomeFragment.java
+++ app/src/main/java/com/acdos/comp41690/ui/home/HomeFragment.java
@@ -5,14 +5,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.acdos.comp41690.R;
+import com.acdos.comp41690.ui.rain.RainFragment;
+import com.acdos.comp41690.ui.solar.SolarFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -31,5 +36,32 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    public void showRainTransition(View view) {
+        displayToast(getString(R.string.menu_rain));
+    }
+    public void showSolarTransition(View view) {
+        displayToast(getString(R.string.menu_solar));
+    }
+    public void solarTransition(){
+        Fragment solarFragment = new SolarFragment();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_solar, solarFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+    public void rainTransition(){
+        Fragment rainFragment = new RainFragment();
+        FragmentManager rainFragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction rainFragmentTransaction = rainFragmentManager.beginTransaction();
+        rainFragmentTransaction.replace(R.id.nav_rain, rainFragment);
+        rainFragmentTransaction.addToBackStack(null);
+        rainFragmentTransaction.commit();
+    }
+    public void displayToast(String message) {
+        Toast.makeText(getActivity(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
