a60ee4f4ad575e9771784f8f338ef459390ba7d9
AfroAl <toandrewlambert@gmail.com>
2019-11-19 15:35:40 +0000

    - Moved Solar functionality to ui.solar & deleted electricity_ui folder - Created app_bar_solar, content_solar, activity_solar_drawer, solar, and mobile_nav_solar to work with Navigation drawer - Modified activity_electricity to follow along with the new files

--- app/src/main/java/com/acdos/comp41690/ElectricityActivity.java
+++ app/src/main/java/com/acdos/comp41690/ElectricityActivity.java
@@ -1,21 +1,95 @@
 package com.acdos.comp41690;
 
+import android.content.Intent;
 import android.os.Bundle;
-import com.acdos.comp41690.electricity_ui.SectionsPagerAdapter;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.acdos.comp41690.ui.solar.SectionsPagerAdapter;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ElectricityActivity extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electricity);
+
+        //Add Tab stuff
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        //Add Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar_solar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_solar);
+        NavigationView navigationView = findViewById(R.id.nav_view_solar);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_rain, R.id.nav_solar,
+                R.id.nav_settings)
+                .setDrawerLayout(drawer)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_solar);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                if(menuItem.isChecked()) {
+                    assert mAppBarConfiguration.getDrawerLayout() != null;
+                    mAppBarConfiguration.getDrawerLayout().closeDrawer(GravityCompat.START);
+                    return false;
+                }
+
+                //Go to Home Section
+                if(id == R.id.nav_home) {
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    assert mAppBarConfiguration.getDrawerLayout() != null;
+                    mAppBarConfiguration.getDrawerLayout().closeDrawer(GravityCompat.START);
+                    startActivity(i);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.solar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_solar);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
