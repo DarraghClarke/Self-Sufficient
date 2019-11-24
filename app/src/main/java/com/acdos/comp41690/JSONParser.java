f64ad3e07c952af66691830d93be2914ded3254f
charliekelly13 <charleskelly13@gmail.com>
2019-11-24 15:42:54 +0000

    Starting json implementation

--- app/src/main/java/com/acdos/comp41690/JSONParser.java
+++ app/src/main/java/com/acdos/comp41690/JSONParser.java
@@ -1,4 +1,63 @@
 package com.acdos.comp41690;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JSONParser {
+
+    public static WeatherStore getWeather(String data) throws JSONException  {
+        WeatherStore weather = new WeatherStore();
+
+        JSONObject jObj = new JSONObject(data);
+
+
+        JSONObject sysObj = getObject("sys", jObj);
+        weather.setSunrise(getInt("sunrise", sysObj));
+        weather.sunTimes.setSunset(getInt("sunset", sysObj));
+
+        JSONArray jArr = jObj.getJSONArray("weather");
+
+        // We use only the first value
+        JSONObject JSONWeather = jArr.getJSONObject(0);
+        weather.currentCondition.setWeatherId(getInt("id", JSONWeather));
+        weather.currentCondition.setDescr(getString("description", JSONWeather));
+        weather.currentCondition.setCondition(getString("main", JSONWeather));
+        weather.currentCondition.setIcon(getString("icon", JSONWeather));
+
+        JSONObject mainObj = getObject("main", jObj);
+        weather.currentCondition.setHumidity(getInt("humidity", mainObj));
+        weather.currentCondition.setPressure(getInt("pressure", mainObj));
+        weather.temperature.setMaxTemp(getFloat("temp_max", mainObj));
+        weather.temperature.setMinTemp(getFloat("temp_min", mainObj));
+        weather.temperature.setTemp(getFloat("temp", mainObj));
+
+        // Wind
+        JSONObject wObj = getObject("wind", jObj);
+        weather.wind.setSpeed(getFloat("speed", wObj));
+        weather.wind.setDeg(getFloat("deg", wObj));
+
+
+
+        return weather;
+    }
+
+
+    private static JSONObject getObject(String tagName, JSONObject jObj)  throws JSONException {
+        JSONObject subObj = jObj.getJSONObject(tagName);
+        return subObj;
+    }
+
+    private static String getString(String tagName, JSONObject jObj) throws JSONException {
+        return jObj.getString(tagName);
+    }
+
+    private static float  getFloat(String tagName, JSONObject jObj) throws JSONException {
+        return (float) jObj.getDouble(tagName);
+    }
+
+    private static int  getInt(String tagName, JSONObject jObj) throws JSONException {
+        return jObj.getInt(tagName);
+    }
+
 }
---
+++ app/src/main/java/com/acdos/comp41690/WeatherStore.java
@@ -0,0 +1,172 @@
+package com.acdos.comp41690;
+
+
+
+public class WeatherStore {
+
+    public CurrentCondition currentCondition = new CurrentCondition();
+    public Temperature temperature = new Temperature();
+    public Wind wind = new Wind();
+    public Rain rain = new Rain();
+
+    public byte[] iconData;
+
+    public class CurrentCondition {
+        private int weatherId;
+        private String condition;
+        private String descr;
+        private String icon;
+
+        private float pressure;
+        private float humidity;
+
+        public int getWeatherId() {
+            return weatherId;
+        }
+
+        public void setWeatherId(int weatherId) {
+            this.weatherId = weatherId;
+        }
+
+        public String getCondition() {
+            return condition;
+        }
+
+        public void setCondition(String condition) {
+            this.condition = condition;
+        }
+
+        public String getDescr() {
+            return descr;
+        }
+
+        public void setDescr(String descr) {
+            this.descr = descr;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+
+        public void setIcon(String icon) {
+            this.icon = icon;
+        }
+
+        public float getPressure() {
+            return pressure;
+        }
+
+        public void setPressure(float pressure) {
+            this.pressure = pressure;
+        }
+
+        public float getHumidity() {
+            return humidity;
+        }
+
+        public void setHumidity(float humidity) {
+            this.humidity = humidity;
+        }
+
+
+    }
+
+    public class SunTimes{
+        private int sunrise;
+        private int sunset;
+
+        public float getSunrise() {
+            return sunrise;
+        }
+
+        public void setSunrise(int sunrise) {
+            this.sunrise = sunrise;
+        }
+
+        public float getSunset() {
+            return sunset;
+        }
+
+        public void setSunset(int sunset) {
+            this.sunset = sunset;
+        }
+    }
+
+
+    public class Temperature {
+        private float temp;
+        private float minTemp;
+        private float maxTemp;
+
+        public float getTemp() {
+            return temp;
+        }
+
+        public void setTemp(float temp) {
+            this.temp = temp;
+        }
+
+        public float getMinTemp() {
+            return minTemp;
+        }
+
+        public void setMinTemp(float minTemp) {
+            this.minTemp = minTemp;
+        }
+
+        public float getMaxTemp() {
+            return maxTemp;
+        }
+
+        public void setMaxTemp(float maxTemp) {
+            this.maxTemp = maxTemp;
+        }
+
+    }
+
+    public class Wind {
+        private float speed;
+        private float deg;
+
+        public float getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(float speed) {
+            this.speed = speed;
+        }
+
+        public float getDeg() {
+            return deg;
+        }
+
+        public void setDeg(float deg) {
+            this.deg = deg;
+        }
+
+
+    }
+
+    public class Rain {
+        private String time;
+        private float amount;
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public float getAmount() {
+            return amount;
+        }
+
+        public void setAmount(float amount) {
+            this.amount = amount;
+        }
+
+
+    }
+}
