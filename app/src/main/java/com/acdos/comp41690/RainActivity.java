6e98d3f794b709aea22cd19ed45f33facec27bd6
DarraghClarke <darragh.clarke@ucdconnect.ie>
2019-11-26 18:01:24 +0000

    rough implementation of rain button

--- app/src/main/java/com/acdos/comp41690/RainActivity.java
+++ app/src/main/java/com/acdos/comp41690/RainActivity.java
@@ -1,11 +1,22 @@
 package com.acdos.comp41690;
 
+import android.app.Dialog;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
+import com.acdos.comp41690.data.UserDataDbHelper;
+import com.acdos.comp41690.data.WaterUsageContract;
 import com.acdos.comp41690.ui.rain.RainSectionsPagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
@@ -19,6 +30,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.time.Instant;
 
 public class RainActivity extends AppCompatActivity {
 
@@ -53,6 +66,52 @@ public class RainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_rain);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        final FloatingActionButton addData = findViewById(R.id.addDataWater);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Dialog addDataAlert = new Dialog(RainActivity.this);
+                addDataAlert.setTitle("Current: " + "kWh");
+
+                addDataAlert.setContentView(R.layout.input_data_dialog);
+                final RadioButton usageButton = addDataAlert.findViewById(R.id.usageButton);
+                final RadioButton outputButton = addDataAlert.findViewById(R.id.outputButton);
+                final EditText inputField = addDataAlert.findViewById(R.id.dataInputField);
+
+                final Button submitButton = addDataAlert.findViewById(R.id.submitButton);
+
+                final Button cancelButton = addDataAlert.findViewById(R.id.cancelButton);
+
+                submitButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (inputField.getText().length() == 0) {
+                            Toast.makeText(RainActivity.this, "Input can not be empty", Toast.LENGTH_SHORT).show();
+                        } else if (usageButton.isChecked()) {
+
+                            addToDatabase(true, Integer.valueOf(inputField.getText().toString()));
+                        } else {
+                            addToDatabase(false, Integer.valueOf(inputField.getText().toString()));
+                        }
+                        addDataAlert.cancel();
+                        finish();
+                        overridePendingTransition(0, 0);
+                        startActivity(getIntent());
+                        overridePendingTransition(0, 0);
+                    }
+                });
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        addDataAlert.cancel();
+                    }
+                });
+
+                addDataAlert.show();
+            }
+        });
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -98,5 +157,20 @@ public class RainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_rain);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    /**
+     * @param dataType if true then solar usage should be used, if false then solar generation contract should be used
+     */
+    private void addToDatabase(boolean dataType, int input) {
+        UserDataDbHelper userDataDbHelper = new UserDataDbHelper(RainActivity.this);
+        ContentValues value = new ContentValues();
+        SQLiteDatabase userDb = userDataDbHelper.getWritableDatabase();
+
+        //if (dataType == true) {
+        value.put(WaterUsageContract.WaterUsageEntry.COLUMN_NAME_VOLUME, input);
+        value.put(WaterUsageContract.WaterUsageEntry.COLUMN_NAME_TIMESTAMP, Instant.now().getEpochSecond());
+        userDb.insert(WaterUsageContract.WaterUsageEntry.TABLE_NAME, null, value);
+        //}
     }
 }
